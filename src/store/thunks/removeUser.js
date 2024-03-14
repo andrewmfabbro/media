3c5e7f999e5cd777bbab95748cb13022614f99e0
Andrew <andrewmfabbro@gmail.com>
2024-03-14 12:45:18 -0400

    Update removeUser.js

--- src/store/thunks/removeUser.js
+++ src/store/thunks/removeUser.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const removeUser = createAsyncThunk("users/remove", async (user) => {
-  await axios.delete(`http://localhost:3005/users/${user.id}`);
+  await axios.delete(`https://json-server-vercel-pi-jet.vercel.app/users/${user.id}`);
 
   
   return user;
